93c02bef4c36a7537d86574c670e84eb6fdfe1a6
Binary Digitz <borisndoh@gmail.com>
2025-03-11 12:05:04 +0100

    Add redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,20 @@ import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import AppProvider from './context/AppProvider.jsx'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
 import './index.css'
 import './my_css.css'
 import App from './App.jsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppProvider>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </AppProvider>
   </BrowserRouter>
