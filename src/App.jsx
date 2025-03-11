416ba6b7878c1698b02ef59c90084bbe85f93510
Binary Digitz <borisndoh@gmail.com>
2025-03-11 04:10:27 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Home, Login, About, SignUp, Profile } from './index'
 
 function App() {
   return (
     <div>
-      app
+      <header>
+        header
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={ <Home />} />
+          <Route path='/login' element={ <Login />} />
+          <Route path='/about' element={ <About />} />
+          <Route path='/sign-up' element={ <SignUp />} />
+          <Route path='/profile' element={ <Profile />} />
+        </Routes>
+      </main>
+      <footer>
+        footer
+      </footer>
     </div>
   )
 }
---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// PAGES
+export { default as Home } from './pages/Home'
+export { default as Login } from './pages/Login'
+export { default as About } from './pages/About'
+export { default as SignUp } from './pages/SignUp'
+export { default as Profile } from './pages/Profile'
+
+
+
+// UI
+
+
+// UX
